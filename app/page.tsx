25906c04932d969f7be08bac490677a0ceebf58b
{sabakanz} <{tomoyam0628@gmail.com}>
2026-04-22 00:55:11 +0900

    画像取得の関数を定義

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-export default function Home() {
+import { connection } from "next/server";
+import { fetchImage } from "./fetch-image";
 
 
+export default async function Home() {
+  //           ^^^^^(1) asyncキーワードを追加
+  // (2) ビルド時にfetchImageの結果が固定されないようにする
+  await connection();
+  // (3) APIから画像を取得
+  const image = await fetchImage();
+  // (4) 画像URLをコンソールに表示
+  console.log("Home: 画像情報を取得しました", image);
 
-
-
-  
   return <div>猫画像予定地</div>;
 }
